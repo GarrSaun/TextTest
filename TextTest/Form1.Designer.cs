0078de4d47c33787829dadd4ee42c43ed3013f4d
garrsaun <garrsaun253@ed.amdsb.com>
2017-12-18 12:40:57 -0500

    Add project files.

---
+++ TextTest/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace TextTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textLabel
+            // 
+            this.textLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLabel.Location = new System.Drawing.Point(12, 185);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(494, 110);
+            this.textLabel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TextTest.Properties.Resources.Class;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(518, 304);
+            this.Controls.Add(this.textLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label textLabel;
+    }
+}
+
---
+++ TextTest/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TextTest
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+        Boolean nextEnabled = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (nextEnabled == true)
+            {
+                scene++;
+                nextEnabled = false;
+
+                switch (scene)
+                {
+                    case 1:
+                        type("(Crash)");
+                        break;
+                    case 2:
+                        textLabel.Text = "Austria-Hungary:";
+                        type(" Kyaaaaaaaaaa!");
+                        break;
+                    case 3:
+                        textLabel.Text = "Austria-Hungary:";
+                        type(" Help me Senpai!");
+                        break;
+                }
+            }
+        }
+
+        public void type(string text)
+        {
+            for (int i = 0; i < text.Length; i++) 
+            {
+                textLabel.Text += text.ElementAt(i);
+                textLabel.Refresh();
+                Thread.Sleep(100);
+            }
+
+            nextEnabled = true;
+        }
+
+    }
+
+
+}
